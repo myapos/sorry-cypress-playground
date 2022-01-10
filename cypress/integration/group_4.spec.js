925a8adc39771a6d1ce412cab4e3f2df8b952236
Myron Apostolakis <myron.apostolakis@sunlight.io>
2022-01-11 00:36:34 +0200

    first commit

---
+++ cypress/integration/group_4.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe("Group 4", () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit("http://localhost:3000");
+  });
+
+  it("should be true - 1", () => {
+    expect(true).to.be.true;
+  });
+
+  it("should be true - 2", () => {
+    expect(true).to.be.true;
+  });
+
+  it("should be true - 3", () => {
+    expect(true).to.be.true;
+  });
+
+  it("should be true - 4", () => {
+    expect(true).to.be.true;
+  });
+});
